88bff8104d43bd9fd460ca0a9c355d71f55dc9d2
Nhat Nam <NhatNam050214@gmail.com>
2026-02-07 00:02:38 +0700

    Add Swagger UI doc for Pentest later

---
+++ src/main/java/com/devops/inventory_service/Config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.devops.inventory_service.Config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Shin Inventory API Service")
+                        .version("V3.0")
+                        .description("API document cho hệ thống quản lý kho Inventory - Made by Shin DevSecOps"))
+                .servers(List.of(
+                        new Server().url("https://shin-devops.me").description("Production Server"),
+                        new Server().url("http://localhost:8080").description("Local Development")
+                ));
+    }
+}
